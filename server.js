43865c605f85844b193a0982b4b62836eb66191a
mahaelab <melabbadi@mail.valenciacollege.edu>
2024-09-04 00:43:11 -0400

    Initial commit

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the main index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Set the port to use, default to 3000
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
